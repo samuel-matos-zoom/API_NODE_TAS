f145598dcb4093d6272e9f8e5d3f65b873d43143
Samuel Arná <samuel.matos@zoomeducacaocorporativa.com.br>
2023-10-31 17:14:32 -0300

    (FIX): Relatorios

--- modules/matriculas/routes/index.js
+++ modules/matriculas/routes/index.js
@@ -10,11 +10,10 @@ exports.matriculasRoutes = (app) => {
         execSQLQuery(sqlQry, cb, req.body.cliente);
     });
 
-
     //status e data end
     app.post('/setProgressoMatricula', (req, res) => {
         var sqlQry = [];
-        var agora = moment().format('YYYY-MM-DDTHH:mm:ss') + '.000Z';
+        var agora = moment().subtract({ hours: 3 }).format('YYYY-MM-DDTHH:mm:ss') + '.000Z';
         switch (req.body.porcentagem) {
             case "0":
                 sqlQry = ["UPDATE curso_matricula SET progresso = ?, data_start = ? WHERE id_usuario = ? AND id_curso = ?", [req.body.porcentagem, agora, req.body.idUser, req.body.idCurso]];
